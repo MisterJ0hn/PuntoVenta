365b4ae0b68c6710db5a25d238f349e21f21e64b
Jonathan Romero <jonathan.romero@gmail.com>
2014-10-02 19:18:06 -0300

    Ingreso de Documentos

---
+++ InventarioWeb/admin/IngresoDocumento.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ADM.Master" AutoEventWireup="true" CodeBehind="IngresoDocumento.aspx.cs" Inherits="InventarioWeb.admin.IngresoDocumento" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<h1>Datos de Factura</h1>
+<asp:TextBox ID="Codigo" runat="server"  OnTextChanged="Codificar"   ></asp:TextBox>
+<p>
+<asp:Label ID="lbl1" runat="server" Text="Rut Proveedor" Width="150"></asp:Label>
+<asp:TextBox ID="txtRutEmpresa" runat="server"></asp:TextBox>
+</p>
+<p>
+<asp:Label ID="Label1" runat="server" Text="Nombre Proveedor" Width="150"></asp:Label>
+<asp:TextBox ID="txtNombreProveedor" runat="server"></asp:TextBox>
+</p>
+<p>
+<asp:Label ID="Label2" runat="server" Text="Numero Factura" Width="150"></asp:Label>
+<asp:TextBox ID="txtNumero" runat="server"></asp:TextBox>
+</p>
+<p>
+<asp:Label ID="Label4" runat="server" Text="Fecha Vencimiento" Width="150"></asp:Label>
+<asp:TextBox ID="txtFechaVenc" runat="server"></asp:TextBox>
+</p>
+<p>
+<asp:Label ID="Label3" runat="server" Text="Monto" Width="150"></asp:Label>
+<asp:TextBox ID="txtMonto" runat="server"></asp:TextBox>
+</p>
+<h2>Prueba Caputador Virtual</h2>
+<asp:TextBox ID="Capturadora" runat="server"></asp:TextBox> 
+    <asp:Button ID="btnCapturadora" runat="server" Text="Capturadora" 
+        onclick="btnCapturadora_Click" />
+
+</asp:Content>
---
+++ InventarioWeb/admin/IngresoDocumento.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InventarioWeb.admin
+{
+    public partial class IngresoDocumento : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Codigo.Focus();
+        }
+       
+        protected void btnCapturadora_Click(object sender, EventArgs e)
+        {
+            Codigo.Text=Capturadora.Text;
+
+        }
+
+        protected void Codificar(object sender, EventArgs e)
+        {
+            String codigo;
+            String[] array;
+            txtRutEmpresa.Text = "";
+            txtNombreProveedor.Text = "";
+            txtNumero.Text = "";
+            txtFechaVenc.Text = "";
+            txtMonto.Text = "";
+            codigo = Codigo.Text;
+            array = codigo.Split(new char[] { ';' });
+            foreach (String str in array)
+            {
+                String[] subArray = str.Split(new char[] { ':' });
+                if (subArray[0].ToString() == "RE")
+                {
+                    txtRutEmpresa.Text = subArray[1].ToString().Replace('\'', '-');
+                }
+                if (subArray[0].ToString() == "RS")
+                {
+                    txtNombreProveedor.Text = subArray[1].ToString();
+                }
+                if (subArray[0].ToString() == "F")
+                {
+                    txtNumero.Text = subArray[1].ToString();
+                }
+                if (subArray[0].ToString() == "FE")
+                {
+                    txtFechaVenc.Text = subArray[1].ToString().Replace('\'', '-');
+                }
+                if (subArray[0].ToString() == "MNT")
+                {
+                    txtMonto.Text = subArray[1].ToString();
+                }
+                //Console.WriteLine(subArray[0].ToString() + " " + subArray[1].ToString());
+            }
+        }
+    }
+}
