dc67a19d583fb99745ad7ebe813ea9d044b5ac8c
Jonathan Romero PTI <jromero@proyectosti.cl>
2014-11-13 18:19:35 -0300

    Promociones

---
+++ InventarioWeb/admin/AgregarPromocion.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ADM.Master" AutoEventWireup="true" CodeBehind="AgregarPromocion.aspx.cs" Inherits="InventarioWeb.admin.AgregarPromocion" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Agregar Promocion</h1>
+<div  style="border:1px">
+<p>
+<asp:Label ID="lblNombre" Text="Nombre" runat="server"></asp:Label>
+<asp:TextBox ID="txtNombre" runat="server" AutoPostBack="true" ></asp:TextBox>
+</p>
+<p>
+<asp:Label ID="Label1" Text="Codigo" runat="server"></asp:Label>
+<asp:TextBox ID="txtCodigo" runat="server"  AutoPostBack="true" ></asp:TextBox>
+</p>
+<p>
+<asp:Label ID="Label2" Text="Precio" runat="server"></asp:Label>
+<asp:TextBox ID="txtPrecio" runat="server"  AutoPostBack="true" ></asp:TextBox>
+</p>
+</div>
+
+<div>
+<p>
+    <asp:HiddenField ID="hdIdProducto" runat="server" />
+</p>
+<p>
+    <asp:Label ID="lblCodigo" Text="Codigo" runat="server"></asp:Label>
+    <asp:TextBox ID="txtCodigoDetalle" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="Label3" Text="Descripcion" runat="server"></asp:Label>
+    <asp:TextBox ID="txtDescripcion" runat="server"></asp:TextBox>
+</p>
+<p>
+    <asp:Label ID="lblCantidad" Text="Cantidad" runat="server"></asp:Label>
+    <asp:TextBox ID="txtCantidad" runat="server"></asp:TextBox>
+    <input id="Reset1" type="reset" value="reset" /></p>
+
+<p class="submitButton">
+    <asp:Button ID="btnAgregar" Text="Guardar" runat="server" 
+        onclick="btnAgregar_Click" />
+</p>
+<p>
+    <asp:GridView ID="GridProductos" runat="server">
+    </asp:GridView>
+</p>
+</div>
+<asp:Button ID="btnFinalizar" Text="Finalizar" runat="server" />
+</asp:Content>
---
+++ InventarioWeb/admin/AgregarPromocion.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using InventarioWebApp;
+using System.Collections;
+
+namespace InventarioWeb.admin
+{
+    public partial class AgregarPromocion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["dt"] = "";
+
+
+            
+        }
+       
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (Session["dt"] == null || Session["dt"] == "")
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("IdProducto", typeof(Int32));
+                dt.Columns.Add("Codigo", typeof(String));
+                dt.Columns.Add("Nombre", typeof(String));
+                dt.Columns.Add("Cantidad", typeof(Int32));
+                DataRow Row1;
+                Row1 = dt.NewRow();
+                Row1["IdProducto"] = Convert.ToInt32(hdIdProducto.Value);
+                Row1["Codigo"] = txtCodigoDetalle.Text;
+                Row1["Nombre"] = txtDescripcion.Text;
+                Row1["Cantidad"] =Convert.ToInt32(txtCantidad.Text);
+                dt.Rows.Add(Row1);
+                GridProductos.DataSource = dt;
+                GridProductos.DataBind();
+                Session["dt"] = dt;
+            }
+            else
+            {
+                DataTable dt = (Session["dt"]) as DataTable;
+                DataRow Row1;
+                Row1 = dt.NewRow();
+                Row1["IdProducto"] = Convert.ToInt32(hdIdProducto.Value);
+                Row1["Codigo"] = txtCodigoDetalle.Text;
+                Row1["Nombre"] = txtDescripcion.Text;
+                Row1["Cantidad"] = Convert.ToInt32(txtCantidad.Text);
+                dt.Rows.Add(Row1);
+                GridProductos.DataSource = dt;
+                GridProductos.DataBind();
+                Session["dt"] = dt;
+            }
+        }
+
+        protected void txtCodigoDetalle_TextChanged(object sender, EventArgs e)
+        {
+            AppDocumentos appDocumentos = new AppDocumentos();
+            ArrayList arrProd = new ArrayList();
+            arrProd = appDocumentos.DetalleProductoBuscar(txtCodigoDetalle.Text, Convert.ToInt32(Session["idSucursal"].ToString()));
+            if (arrProd.Count > 0)
+            {
+                hdIdProducto.Value = arrProd[0].ToString();
+                txtDescripcion.Text = arrProd[1].ToString();
+                txtCantidad.Text = "1";
+            }
+            else
+            {
+                //vldCodigo.Visible = true;
+                hdIdProducto.Value = "";
+                txtDescripcion.Text = "";
+                txtCantidad.Text = "0";
+                txtCodigoDetalle.Text = "0";
+                
+            }
+        }
+
+         
+
+    }
+}
